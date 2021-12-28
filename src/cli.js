ddf6853c09f09a4af538b1ac5c70ae1cf3bfb64b
jassmin-herr <jassminfernandez@gmail.com>
2021-12-27 19:25:37 -0600

    'delete config and utils file, delete comments'

--- src/cli.js
+++ src/cli.js
@@ -1,24 +1,7 @@
-/* 
- -index.js tells Node that our app will use ES6 Modules and gets the cli function to process user inputs.
- - the cli.js file then handles user commands by leveraging the tool minimist. 
- - the tool minimist converts the arguments into an object. 
-    - example: my input books search, console.log(argsObj) output ->   { _: [ 'search' ] }
- - each file in src folder for each command.   
-*/
-
 import minimist from "minimist";
 import { help } from './help';
 import { version } from './version';
 import {search} from './search';
-import { configure } from './configure';
-
-// A list of commands that the CLI app will process along with expected commands : version, help. 
-  
-/* search
-    save
-    config
-    list
-    default */
 
 export async function cli(argsArray) {
     const args = minimist(argsArray.slice(2));
