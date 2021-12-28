e548c2a1e486dae7965c3d13b4a676613208432e
jassmin-herr <jassminfernandez@gmail.com>
2021-12-27 19:59:56 -0600

    'created bin file, remvd chalk, esm, reset files'

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-#!/usr/bin/env node
+const minimist = require('minimist');
+const {version} = require('./package.json');
 
-require = require('esm')(module);
-require('./src/cli').cli(process.argv);
 
+
+module.exports = () => {
+    
+    const args = minimist(argsArray.slice(2));
+    let cmd = args._[0] || 'help';
+  
+    if (args.version || args.v) {
+      cmd = 'version';
+    }
+    if (args.help || args.h) {
+      cmd = 'help';
+    }
+  
+    
+    switch (cmd) {
+      case 'version':
+        require('./cmds/version')(args, version);
+        break;
+  
+      case 'help':
+        help(args);
+        break;
+  
+      case 'search':
+        search(args);
+        break;
+  
+      case 'save':
+        save(args);
+        break;
+  
+      case 'config':
+        configure(args);
+        break;
+
+      case 'list':
+        list(args);
+        break;
+  
+      default:
+        console.error(`"${cmd}" is not a valid command!`);
+        break;
+    }
+  }
